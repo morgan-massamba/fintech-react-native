6a8c2f90dce3467c4943bccafd18fd5e90e0b240
Morgan <chabcontact@gmail.com>
2024-04-02 18:56:32 +0200

    feat: wip face id

--- app/(tabs)/invest.tsx
+++ app/(tabs)/invest.tsx
@@ -1,7 +1,26 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+
+// Face ID
+import * as LocalAuthentication from "expo-local-authentication";
 
 export default function Page() {
+  useEffect(() => {
+    const requestFaceId = async () => {
+      try {
+        const { success } = await LocalAuthentication.authenticateAsync();
+        if (success) {
+          console.log("access OK");
+        } else {
+          console.log("access refused");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    requestFaceId();
+  }, []);
+
   return (
     <View>
       <Text>Invest Page</Text>
